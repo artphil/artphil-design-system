9c566b38ad1d913f3247e0917c22a1ba9bf369b4
Arthur Phillip Silva <artphil.dev@gmail.com>
2026-05-24 14:22:51 -0300

    feat/create a doc page

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # artphil-design-system
-Design System padrão para meus projetos 
+
+Design System padrão para meus projetos
+
+## Instalação
+
+npm install artphil-design-system
+
+## Uso
+
+import 'artphil-design-system';
+
+## Cores
+
+- --color-primary
+- --color-danger
---
+++ docs/doc.js
@@ -0,0 +1,71 @@
+const DS_PREFIX = "--ap-";
+
+/* Fluxo principal */
+
+// Executa depois que o CSS carregar
+window.addEventListener("load", renderComponents);
+
+/* Funções */
+
+function renderComponents() {
+  renderColors();
+}
+
+function toggleTheme() {
+  const html = document.documentElement;
+  const current = html.getAttribute("data-theme");
+  const next = current === "dark" ? "light" : "dark";
+  html.setAttribute("data-theme", next);
+}
+
+/* Funções auxiliares */
+
+function renderColors() {
+  const container = document.getElementById("colors-grid");
+  const colors = getCSSVariables(DS_PREFIX + "color");
+
+  container.innerHTML = "";
+
+  colors.forEach(({ name, value }) => {
+    const card = createColorCard(name, value);
+    container.appendChild(card);
+  });
+}
+
+function getCSSVariables(prefix) {
+  const styles = getComputedStyle(document.documentElement);
+  const vars = [];
+
+  for (let i = 0; i < styles.length; i++) {
+    const name = styles[i];
+
+    if (name.startsWith(prefix)) {
+      const value = styles.getPropertyValue(name).trim();
+      vars.push({ name, value });
+    }
+  }
+
+  return vars;
+}
+
+function createColorCard(name, value) {
+  const card = document.createElement("div");
+  card.className = "color-card";
+
+  const preview = document.createElement("div");
+  preview.className = "color-preview";
+  preview.style.background = value;
+
+  const info = document.createElement("div");
+  info.className = "color-info";
+  info.innerHTML = `
+      <strong>${name.replace(DS_PREFIX + "color-", "")}</strong><br/>
+      ${name}<br/>
+      ${value}
+    `;
+
+  card.appendChild(preview);
+  card.appendChild(info);
+
+  return card;
+}
